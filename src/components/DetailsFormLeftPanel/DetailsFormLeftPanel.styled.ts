3fc1dbd8e31f42fea557675e8b07ec794e8eebc1
sameendraK <sameendrak@gmail.com>
2024-06-23 13:21:31 +0530

    feat(DetailsFormLeftPanel, DetailsFormRightPanel, App): Add dynamic styling using flexbox layout.

--- src/components/DetailsFormLeftPanel/DetailsFormLeftPanel.styled.ts
+++ src/components/DetailsFormLeftPanel/DetailsFormLeftPanel.styled.ts
@@ -11,10 +11,15 @@ margin-left:16px
 }
 
 .details-form-container-wrapper{
-    width:20%;
+    width:70%;
     position: relative;
     top: 200px;
-    left: 150px;
+    left: 100px;
+    border-radius: 10px;
+    color:white;
+    background: rgba(0, 0, 0, 0.31);
+    // height:150px;
+    // box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.15);
     }
 
     .logo-container{
@@ -25,5 +30,35 @@ margin-left:16px
         background:#2BB4C0;
         border-radius:50%;
         color:white;
+    }
+
+    .nav-items-container{
+    height: 100%;
+    display: flex;
+    }
+
+    .active-tab{
+        background:green;
+        color:yellow;
+    }
+
+    li{
+    list-style:none;
+    }
+
+    .react-icons{
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+    }
+    .nav-headings{
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-bottom:0;
+    }
+
+    ul{
+    display:flex;
     }
 `;
--- src/components/DetailsFormRightPanel/DetailsFormRightPanel.styled.ts
+++ src/components/DetailsFormRightPanel/DetailsFormRightPanel.styled.ts
@@ -9,16 +9,67 @@ input:focus{
 border:0px;
 border-bottom:solid 1px black;
 }
+
 .details-form-right-panel-wrapper-container{
-position: absolute;
-    left: 550px;
-    font-size: x-large;
-    height: 280px;
-    width: 400px;
-    text-align: center;
-    // box-shadow: 1px 5px 35px 10px #70bcff;
-    border-radius: 11px;
-    top:170px;
-    transform: translate3d(-28px, -225px, 44px);
+display:flex;
+}
+
+.fVDNpt{
+    height:100%
+}
+
+.right-panel-wrapper-container{
+    height: 100%;
+    display: flex;
+}
+
+.panel-1{
+    flex-direction: column;
+    justify-content: space-around;
+    align-self: center;
+}
+
+.panel-2{
+    flex-direction: column;
+    align-self: center;
+    margin-right: auto;
+    margin-left: 30px;
+}
+// .details-form-right-panel-wrapper-container{
+// position: absolute;
+//     left: 550px;
+//     font-size: x-large;
+//     height: 280px;
+//     width: 400px;
+//     text-align: center;
+//     // box-shadow: 1px 5px 35px 10px #70bcff;
+//     border-radius: 11px;
+//     top:100px;
+//     transform: translate3d(-28px, -225px, 44px);
+//     // background: red;
+//     height: 300px;
+//     width: 285px;
+//     align-content: center;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     gap:16px;
+//     }
+
+    .label-class{
+    font-size: 16px;
+    width: 100%;
+    text-align: left;
     }
+
+
+.panel-1, .panel-2 {
+   flex: 1;
+   margin: 0 10px;
+}
+
+.details-form-right-panel-container {
+   margin-bottom: 15px;
+}
+
 `;
